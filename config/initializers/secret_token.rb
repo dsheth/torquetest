c262995bffaae73e65dc953981ea45ed28638e7b
David Sheth <david@dsheth.com>
2012-04-05 17:01:21 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Torquetest::Application.config.secret_token = '144738148ed169870504fc0143280dd39a0b66c0c90d08267b6aea9c08d55912aca80af094eaf456d61c110f8d9cf6cbea17c75d3343517344ee8d00fbab0a5f'
