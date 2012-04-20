69c10ccadf3b0e74f3461e2b97919fe9cbb6a053
David Sheth <david@dsheth.com>
2012-04-20 16:33:21 -0500

    add auth to controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate
   
   private
+  
+  def authenticate
+    if current_user.nil?
+	   logger.info "Current User is nill, redirecting"
+	   redirect_to "/"
+	end
+  end
   
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  skip_before_filter :authenticate
 
   def new
     @user = User.new
